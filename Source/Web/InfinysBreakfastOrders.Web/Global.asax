5d2c782e2d4cffefbcde2779c5a94ac4c3207d1c
g-stoyanov <georgi.j.stoyanov@gmail.com>
2015-02-11 14:50:50 +0200

    Created empty data and web projects

---
+++ Source/Web/InfinysBreakfastOrders.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InfinysBreakfastOrders.Web.MvcApplication" Language="C#" %>
---
+++ Source/Web/InfinysBreakfastOrders.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InfinysBreakfastOrders.Web.Startup))]
+namespace InfinysBreakfastOrders.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
